3a2ffeb4bcbac45dedb2738123e357743c461f8a
gygy7151 <gygy7151@gmail.com>
2022-08-25 18:42:50 +0900

    feat: 장바구니 기능 구현완료 추후 상품수량 데이터 추가작업 고도화 필요

--- frontend/static/js/shopping.js
+++ frontend/static/js/shopping.js
@@ -14,11 +14,12 @@ var products = [
 function templateOutput(data) {
     let 템플릿 = "";
     data = sorting(data);
-    data.forEach(a => {
+    data.forEach((a, i) => {
         템플릿 += `<div class="col-sm-4">
         <img src="https://via.placeholder.com/600" class="w-100" />
         <h5>${a.title}</h5>
-        <p>가격 : ${a.price}.price}</p>
+        <p>가격 : ${a.price}</p>
+        <button class="buy-button" data-id="${i}">구매</button>
         </div>`;
     });
     return 템플릿;
@@ -82,4 +83,43 @@ function sortingRe(arr, key) {
     return arr;
 }
 
-console.log(sorting(products));
+document.querySelector(".row").addEventListener("click", function (e) {
+    // 구매버튼을 누르면 로컬스토리지에 선택한 상품이 있는지 확인
+    // 구매버튼을 클릭한 상품명을 가져온다.
+    // key명을 cart로 정하자.
+    // 로컬스토리지에 구매를 클릭한 상품명이 없으면 상품명을 추가해준다
+    // 만약있으면
+    const childs = document.querySelector(".row").childNodes;
+    let childOrder = e.target.dataset.id;
+    const nameOfProduct = childs[childOrder].childNodes[3].innerHTML;
+    console.log(
+        e.target.previousElementSibling.previousElementSibling.innerHTML
+    ); //이렇게 두개로 접근도 가능함
+
+    // - !표로 해주면 true반대인 false가 아님
+    if (localStorage.hasOwnProperty("cart") == false) {
+        localStorage.setItem("cart", JSON.stringify([nameOfProduct]));
+        // - 값이 있으면 값을 객체 변수에 담고 새로 추가할 상품
+    } else {
+        let nowCart = JSON.parse(localStorage.getItem("cart"));
+        // nowCart에 값이 없으면 추가해준다
+
+        if (nowCart.indexOf(nameOfProduct) == -1) {
+            nowCart.push(nameOfProduct);
+        }
+        localStorage.removeItem("cart");
+        localStorage.setItem("cart", JSON.stringify(nowCart));
+    }
+});
+//만약 없으면
+
+// for (let i = 0; i < localStorage.length; i++) {
+//     let key = localStorage.key(i);
+//     console.log(key);
+//     console.log(localStorage.getItem(key));
+// }
+
+// - 장바구니 버튼 클릭시 특정 페이지로 이동
+document.querySelector("#basket").addEventListener("click", function () {
+    window.location.href = "./basket.html";
+});
